0f71ff868d8b271a113d986127b330480f7b496a
Hugo Friedrich <86070257+Hugofriedrich369@users.noreply.github.com>
2023-03-21 01:41:26 -0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  Prefix: "i.",
+  Users: {
+    OWNERS: ["950168815407333419"]
+  },
+  Handlers: {
+    MONGO: "mongodb+srv://InfiStore:123@infistore.2yqn6fo.mongodb.net/test"
+  },
+  Client: {
+    TOKEN: "MTA2NTc0MjcyNjIzMjAzNTQyOA.GKkPGJ.a3i7nk3AK1E-x6hT-sbBr-bM_0lH0qqxfRmkhk",
+    ID: "1065742726232035428"
+  }
+}
