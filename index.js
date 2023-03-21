093e82ae1b124896980247bc4a7b4fe8954a8954
Hugo Friedrich <86070257+Hugofriedrich369@users.noreply.github.com>
2023-03-21 01:37:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const { Client, Partials, Collection, GatewayIntentBits } = require('discord.js');
+const config = require('./config/config');
+const colors = require("colors");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction
+  ],
+  presence: {
+    activities: [{
+      name: "InfiStore - Administração",
+      type: 0
+    }],
+    status: 'dnd'
+  }
+});
+
+require('http').createServer((req, res) => res.end('Ready.')).listen(3000);
+
+const AuthenticationToken = process.env.TOKEN || config.Client.TOKEN;
+if (!AuthenticationToken) {
+  return process.exit();
+};
+
+client.prefix_commands = new Collection();
+client.slash_commands = new Collection();
+client.user_commands = new Collection();
+client.message_commands = new Collection();
+client.modals = new Collection();
+client.events = new Collection();
+
+module.exports = client;
+
+["prefix", "application_commands", "modals", "events", "mongoose"].forEach((file) => {
+  require(`./handlers/${file}`)(client, config);
+});
+
+client.login(AuthenticationToken)
+  .catch((err) => {
+    console.error("[CRASH] Something went wrong while connecting to your bot...");
+    console.error("[CRASH] Error from Discord API:" + err);
+    return process.exit();
+  });
+
+process.on('unhandledRejection', async (err, promise) => {
+  console.error(`[ANTI-CRASH] Unhandled Rejection: ${err}`.red);
+  console.error(promise);
+});
